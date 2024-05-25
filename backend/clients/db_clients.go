b050525c8041129f72b9d7ad427d9be8f4f7b9bb
arnonahmias <2222270@ucc.edu.ar>
2024-05-25 15:21:26 -0300

    clients

--- backend/clients/db_clients.go
+++ backend/clients/db_clients.go
@@ -1,1 +1,41 @@
 package clients
+
+import (
+	"backend/dao"
+	"fmt"
+	"time"
+)
+
+const (
+	tableNameUsers         = "users"
+	tableNameCourses       = "courses"
+	tableNameSubscriptions = "subscriptions"
+)
+
+func main() {
+	db := map[string][]interface{}{
+		tableNameUsers: {
+			dao.User{
+				ID:           1,
+				Email:        "arnonahmias13@gmail.com",
+				PasswordHash: "5f4dcc3b5aa765d61d8327deb882cf99",
+				Type:         "admin",
+				CreationDate: time.Now().UTC(),
+				LastUpdated:  time.Now().UTC(),
+			},
+
+			dao.User{
+				ID:           2,
+				Email:        "juanperez@gmail.com",
+				PasswordHash: "5f4dcc3b5aa765d61d8327deb882cf99",
+				Type:         "normal",
+				CreationDate: time.Now().UTC(),
+			},
+		},
+	}
+
+	// ... Additional user data and operations
+	users := db[tableNameUsers].([]dao.User)
+	fmt.Println("User 1:", users[0])
+	fmt.Println("User 2:", users[1])
+}
