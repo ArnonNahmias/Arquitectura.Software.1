fef5b56b39e044f54a6bc992a71a61841b1cb42e
arnonahmias <2222270@ucc.edu.ar>
2024-05-25 15:28:40 -0300

    login, user y controllers

---
+++ backend/dao/user.go
@@ -0,0 +1,13 @@
+package dao
+
+import "time"
+
+// User representa un usuario en el sistema.
+type User struct {
+	ID           int64     // ID del usuario
+	EmailHash    string    // Hash del correo electrónico del usuario
+	PasswordHash string    // Hash de la contraseña del usuario
+	Type         string    // Tipo de usuario (admin o normal)
+	CreationDate time.Time // Fecha de creación del usuario
+	LastUpdated  time.Time // Fecha de última actualización del usuario
+}
