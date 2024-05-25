423b7e8e99b2f6b09fe9bfdb217a1d97fd404a41
arnonahmias <2222270@ucc.edu.ar>
2024-05-25 14:46:10 -0300

    login

---
+++ backend/services/login.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"backend/domain"
+	"backend/services"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func Login(email string, password string) (string, error) {
+	if strings.TrimSpace(email) == "" {
+		return "", errors.New("Email is required")
+	}
+
+	if strings .TrimSpace(password) == "" {
+		return "", errors.New("Password is required")
+	}
+
+	hash := fmt.Spintff("%x", md5.sum([]byte(password)))
+
+	userDAO, err := services.GetUserByEmail(email)
+	if err != nil {
+		return "", fmt.Errorf("invalid credentials")
+	}
+
+	//TODO: Replace this with JWT token generation
+	token := hash
+
+	return,nill
+}
