87ab178072de729fd48ea5103019fb6e1a0b1c1d
arnonahmias <2222270@ucc.edu.ar>
2024-05-31 18:16:32 -0300

    correciones del emi

--- backend/controllers/users_controller.go
+++ backend/controllers/users_controller.go
@@ -1,16 +1,1 @@
-package main
-
-import (
-	usersDomain "backend/domain/users"
-	usersService "backend/services/users"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func Login(context *gin.Context) {
-	var loginRequest usersDomain.LoginRequest
-	context.BindJSON(&loginRequest)
-	response := usersService.Login(loginRequest)
-	context.JSON(http.StatusOK, response)
-}
+package controllers
