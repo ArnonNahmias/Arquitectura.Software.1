5dd7d4a1c632cc4c088715321669b376e88269e2
felipeganame <2111762@ucc.edu.ar>
2024-06-01 11:11:53 -0300

    avanzando

---
+++ frontend/src/components/CommonUsertView/CommonUserView.js
@@ -0,0 +1,14 @@
+// src/components/CommonUserView/CommonUserView.js
+import React from 'react';
+import './CommonUserView.scss';
+
+const CommonUserView = () => {
+  return (
+    <div className="common-user-view">
+      <h2>Welcome, User!</h2>
+      <p>This is the common user view.</p>
+    </div>
+  );
+};
+
+export default CommonUserView;
---
+++ frontend/src/components/CommonUsertView/index.js
@@ -0,0 +1,1 @@
+import CommonUserView from "./CommonUserView";
