e8ce8eb7e324da22aaeca2988311fbe2e4ae802f
felipeganame <2111762@ucc.edu.ar>
2024-07-14 18:58:48 -0300

    docker??

--- backend/clients/db_clients.go
+++ backend/clients/db_clients.go
@@ -1,33 +1,42 @@
 package clients
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"log"
+    "os"
+    "fmt"
+    "log"
 
-	"backend/dao"
+    "backend/dao"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 
-	"errors"
-	"time"
+    "crypto/md5"
+    "encoding/hex"
+    "errors"
+    "time"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	log.Println("Initializing database...")
-	dsn := "root:58005800@tcp(127.0.0.1:3306)/proyecto1?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("failed to connect to database: ", err)
-	}
-	log.Println("Database connected successfully")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
 
-	Migrate()
-	SeedDB()
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+    log.Println("Initializing database...")
+    //dsn := "root:58005800@tcp(127.0.0.1:3306)/proyecto?charset=utf8mb4&parseTime=True&loc=Local"
+    var err error
+    DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("failed to connect to database: ", err)
+    }
+    log.Println("Database connected successfully")
+
+    Migrate()
+    SeedDB()
 }
 
 func Migrate() {
